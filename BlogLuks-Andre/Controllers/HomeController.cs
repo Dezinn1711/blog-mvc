cad411153fa8e979d1e4ddbadfabaa39bb415a7a
Dezinn1711 <andreluizrosa4@gmail.com>
2025-08-11 22:27:58 -0300

    Adicionando Objetos na View

--- BlogLuks-Andre/Controllers/HomeController.cs
+++ BlogLuks-Andre/Controllers/HomeController.cs
@@ -15,31 +15,70 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        ViewData["Mensagem"] = "Olá Mundo!";
         //Criar objetos
-        Categoria categoria = new();
-        categoria.Id = 1;
-        categoria.Nome = "Tecnologia";
+        Categoria Hisoka = new();
+        Hisoka.Id = 1;
+        Hisoka.Nome = "Hisoka Morrow";
 
-        Categoria categoria2 = new() 
+        Categoria killua = new() 
         {
             Id= 2,
-            Nome = "IA"
+            Nome = "Killua Zoldyck"
         };
+
+        Categoria kurapika = new (3,"kurapika");
+
+        Categoria netero = new (4,"Isacc Netero");
 
         List<Postagem> postagens = [
             new (){
                 Id = 1,
-                Nome = "Saiu o CHATGPT 5!!!",
-                CategoriaId = 2,
-                Categoria = categoria2,
+                Nome = "Hisoka Morrow ",
+                CategoriaId = 1,
+                Categoria = Hisoka,
                 DataPostagem = DateTime.Parse("07/08/2025"),
-                Descricao = "Estou com preguiça de pensar nisso",
-                Texto = "Lorem ipsum"
-            }
-        ] 
+                Descricao = "Hisoka é um caçador que é viciado em lutar e fica procurando pessoas fortes para ter um duelo",
+                Texto = "Lorem ipsum",
+                Thumbnail = "/img/1.jpg",
+                Foto = "/img/1.jpg"
+            },
 
-        return View();
+            new (){
+                Id = 2,
+                Nome = "Killua Zoldyck ",
+                CategoriaId = 2,
+                Categoria = killua,
+                DataPostagem = DateTime.Parse("07/08/2025"),
+                Descricao = "Killua veio de uma fámilia assassina e gosta muito de seu amigo Gon",
+                Texto = "Lorem ipsum",
+                Thumbnail = "/img/2.jpg",
+                Foto = "/img/2.jpg"
+            },
+
+            new (){
+                Id = 3,
+                Nome = "Kurapika",
+                CategoriaId = 3,
+                Categoria = kurapika,
+                DataPostagem = DateTime.Parse("07/08/2025"),
+                Descricao = "Kurapika quer vingança pela seu clã que foi morto pela trupe fantasma",
+                Texto = "Lorem ipsum",
+                Thumbnail = "/img/3.jpg",
+                Foto = "/img/3.jpg"
+            },
+            new (){
+                Id = 4,
+                Nome = "Isaac Netero",
+                CategoriaId = 4,
+                Categoria = netero,
+                DataPostagem = DateTime.Parse("07/08/2025"),
+                Descricao = "Netero é um velho muito forte, que lutou contra o Rei Meruem",
+                Texto = "Lorem ipsum",
+                Thumbnail = "/img/4.jpg",
+                Foto = "/img/4.jpg"
+            }
+        ]; 
+        return View(postagens);
     }
 
     public IActionResult Privacy()
